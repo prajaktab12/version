633615ecc4fbf5bc7b897350792a4a7666da0d82
unknown <unknown>
2025-05-20 16:58:29 +0530

    test

---
+++ Action.c
@@ -0,0 +1,24 @@
+//   *****************************************************************************************************************************************
+//   ****   PLEASE NOTE: This is a READ-ONLY representation of the actual script. For editing please press the "Develop Script" button.   ****
+//   *****************************************************************************************************************************************
+
+Action()
+{
+	lr_start_transaction("A101_LaunchPage");
+	truclient_step("1", "Navigate to 'https://blazedemo.com/'", "snapshot=Action_1.inf");
+	lr_end_transaction("A101_LaunchPage",0);
+	truclient_step("2", "Wait 10 seconds", "snapshot=Action_2.inf");
+	lr_start_transaction("A102_FindFlight");
+	truclient_step("3", "Click on Find Flights button", "snapshot=Action_3.inf");
+	lr_end_transaction("A102_FindFlight",0);
+	truclient_step("4", "Wait 10 seconds", "snapshot=Action_4.inf");
+	lr_start_transaction("A103_ChooseFlight");
+	truclient_step("5", "Click on Choose This Flight button", "snapshot=Action_5.inf");
+	lr_end_transaction("A103_ChooseFlight",0);
+	truclient_step("6", "Wait 10 seconds", "snapshot=Action_6.inf");
+	lr_start_transaction("A104_PurchaseFlight");
+	truclient_step("7", "Click on Purchase Flight button", "snapshot=Action_7.inf");
+	lr_end_transaction("A104_PurchaseFlight",0);
+
+	return 0;
+}
